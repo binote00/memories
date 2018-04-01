0ad791e5533ed9c7e38984ff0f794bb9cf745d78
binote00 <jfvanass@gmail.com>
2018-04-01 14:39:55 +0200

    add password pattern + clean

--- form/f_profile.php
+++ form/f_profile.php
@@ -6,24 +6,23 @@
  * Time: 13:23
  */
 
-if($_SESSION){
-    if($_SESSION['id']){
+if ($_SESSION) {
+    if ($_SESSION['id']) {
         $form = new Form();
         $user = new User();
         $user->getUser($_SESSION['id']);
-
-        $content = $form->CreateForm('./app/a_profile_mod.php','POST', 'Profil utilisateur')
+        $content = $form->CreateForm('./app/a_profile_mod.php', 'POST', 'Profil utilisateur')
             ->AddInput('first_name', 'Prénom', 'text', $user->getFirstName())
             ->AddInput('last_name', 'Nom', 'text', $user->getLastName())
             ->AddInput('birth_date', 'Date de Naissance', 'date', $user->getBirthDate())
             ->AddInput('email', 'Email', 'email', $user->getEmail(), '', '', 'required')
             ->AddInput('login', 'Identifiant', 'text', $user->getLogin(), '', '', 'disabled')
-            ->AddInput('pwd', 'Nouveau mot de passe', 'password', '', '')
+            ->AddInput('pwd', 'Nouveau mot de passe', 'password', '', '', '', 'pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"')
             ->AddInput('id', '', 'hidden', $user->getId())
             ->EndForm('Modifier', 'danger');
-    }else{
+    } else {
         header('Location: index.php?view=login');
     }
-}else{
+} else {
     header('Location: index.php?view=login');
 }
