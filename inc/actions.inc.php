ff3f344a96e83141354c628d64f0700330bb1f78
binote00 <jfvanass@gmail.com>
2018-06-23 11:17:02 +0200

    session lifetime refresh

--- inc/actions.inc.php
+++ inc/actions.inc.php
@@ -5,6 +5,8 @@
  * Date: 17-11-17
  * Time: 10:21
  */
+session_destroy();
+ini_set('session.gc_maxlifetime', 3600);
 session_start();
 require_once 'const.inc.php';
 spl_autoload_register(function ($classe) {
